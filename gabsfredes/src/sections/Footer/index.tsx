3f6da67da7eb5160bb13b7e2de9cce84892bb7bb
Gabriel Fredes <gabriel.sfredes@gmail.com>
2023-12-23 23:08:56 -0300

    updt: footer
    full responsive and done

--- gabsfredes/src/sections/Footer/index.tsx
+++ gabsfredes/src/sections/Footer/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { MdOutgoingMail } from "react-icons/md";
 import BarLinks from "../../components/NavBar/BarLinks";
 
 const Footer: React.FC = () => {
@@ -10,17 +11,26 @@ const Footer: React.FC = () => {
     >
       <div className="w-full flex justify-between items-center gap-1 py-4 border-b-2">
         <div className="flex-col text-center items-center">
-          <p className="text-header font-bold text-lg lg:text-xl tracking-wider">
+          <p className="text-header font-bold text-base md:text-lg lg:text-xl tracking-wider">
             &#123;GF&#125;
           </p>
-          <p className="text-header font-semibold text-lg lg:text-lg">
+          <p className="text-header font-semibold text-sm md:text-lg lg:text-lg">
             gabsfredes
           </p>
         </div>
         <div className="flex gap-2">
-          <div className="text-lg text-header flex gap-12">
-            <a href="mailto:gabriel.sfredes@gmail.com">
+          <div className="text-lg text-header flex gap-6 md:gap-12">
+            <a
+              href="mailto:gabriel.sfredes@gmail.com"
+              className="hidden md:block"
+            >
               gabriel.sfredes@gmail.com
+            </a>
+            <a
+              href="mailto:gabriel.sfredes@gmail.com"
+              className="block text-red-500 text-2xl md:hidden"
+            >
+              <MdOutgoingMail />
             </a>
             <a
               className="text-header text-2xl"
@@ -51,9 +61,30 @@ const Footer: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="w-full flex justify-between items-center">
-        <div className="hidden lg:block"><BarLinks isFooter={true}/></div>
-        <p className="text-sm md:text-base lg:text-lg text-header">Design base <a href="https://github.com/pavanmg007" rel="noreferrer" className="gradient" target="_blank">PavanMG</a> | Modificações <a href="https://github.com/gabsfredes" rel="noreferrer" className="gradient" target="_blank">&#123;GF&#125;</a></p>
+      <div className="w-full flex justify-center py-4 items-center">
+        <div className="hidden lg:block">
+          <BarLinks isFooter={true} />
+        </div>
+        <p className="text-sm text-center tracking-wide leading-6 ml-6 md:text-base lg:text-lg lg:text-right text-header">
+          Design original{" "}
+          <a
+            href="https://github.com/pavanmg007"
+            rel="noreferrer"
+            className="gradient"
+            target="_blank"
+          >
+            PavanMG
+          </a>{" "}
+          <br></br>Modificações por{" "}
+          <a
+            href="https://github.com/gabsfredes"
+            rel="noreferrer"
+            className="gradient"
+            target="_blank"
+          >
+            &#123;GF&#125;
+          </a>
+        </p>
       </div>
     </footer>
   );
